0ad792ba303a783582bb23afe0a5c141fb3672c9
Giribabu <Uchugowni@users.noreply.github.com>
2024-02-09 13:20:28 +0530

    dag issue

---
+++ dags/reddit_dag.py
@@ -0,0 +1,49 @@
+import os
+import sys
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from pipelines.aws_s3_pipeline import upload_s3_pipeline
+
+default_args = {
+    'owner': 'Yusuf Ganiyu',
+    'start_date': datetime(2023, 10, 22)
+}
+
+file_postfix = datetime.now().strftime("%Y%m%d")
+
+dag = DAG(
+    dag_id='etl_reddit_pipeline',
+    default_args=default_args,
+    schedule_interval='@daily',
+    catchup=False,
+    tags=['reddit', 'etl', 'pipeline']
+)
+
+start = PythonOperator(
+    task_id="start",
+    python_callable = lambda: print("job started"),
+    dag=dag
+)
+
+# upload to s3
+upload_s3 = PythonOperator(
+    task_id='s3_upload',
+    python_callable=upload_s3_pipeline,
+    dag=dag
+)
+
+end=PythonOperator(
+    task_id="end",
+    python_callable=lambda: print("job ended successfully"),
+    dag=dag
+)
+
+start >> upload_s3 >> end
+
+
+
---
+++ jobs/python/wordcountjob.py
@@ -0,0 +1,13 @@
+from pyspark.sql import SparkSession
+
+spark=SparkSession.builder.appName("pythonwordcount").getOrCreate()
+
+text="Hello Spark Hello Python Hello Airflow"
+
+words=spark.sparkContext.parallelize(text.split(" "))
+wordCounts=words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+ 
+for wc in wordCounts.collect():
+    print(wc[0], wc[1])
+
+spark.stop()
